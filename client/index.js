1b41268b567c72254d5f534760dbb1d7afbaca70
Jared Grippe <jared@deadlyicon.com>
2022-10-11 11:39:48 -0700

    WIP

--- client/index.js
+++ client/index.js
@@ -3,6 +3,10 @@ import Routes from './Routes'
 import theme from './theme'
 
 render({
+  // TODO: this because env not usable in shared componencts
+  APP_NAME: process.env.APP_NAME,
+
+
   Routes,
   theme,
 })
---
+++ server/routes.js
@@ -0,0 +1,5 @@
+export default router => {
+  router.get('/custom/route', (req, res, next) => {
+    res.json({ this_was: 'a custom route', now: Date.now() })
+  })
+}
